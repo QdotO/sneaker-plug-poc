89f1bfb115288a11699cb4386bcfe6a6d6931493
Quincy Obeng <quincyobeng@gmail.com>
2023-02-10 09:21:36 -0600

    feat: Update create account for resusability

--- modules/account/components/CreateAccount.tsx
+++ modules/account/components/CreateAccount.tsx
@@ -3,9 +3,12 @@ import { Formik, Form, Field, ErrorMessage } from 'formik'
 import newAccountSchema from '../constants/newAccountSchema'
 import axios from 'axios'
 
-type Props = {}
+type Props = {
+  onSuccess?: () => void
+  onError?: () => void
+}
 
-const CreateAccount = (props: Props) => {
+const CreateAccount = ({ onSuccess, onError }: Props) => {
   const initialValues = {
     firstName: '',
     lastName: '',
@@ -15,7 +18,7 @@ const CreateAccount = (props: Props) => {
     streetAddress2: '',
     city: '',
     state: '',
-    zipCode: '',
+    zipCode: ''
   }
 
   const onSubmit = async (values, { setSubmitting }) => {
@@ -23,62 +26,120 @@ const CreateAccount = (props: Props) => {
       .post('/api/users/create', {
         account: {
           id: crypto.randomUUID(),
-          ...values,
-        },
+          ...values
+        }
       })
       .catch((e) => {
         console.log({ UserCreateError: e })
+        onError?.()
       })
     setSubmitting(false)
     console.log({ CreateAccountResponse: response })
+    onSuccess?.()
   }
 
   return (
     <div>
-      <h1>Create your new Sneaker Plug account</h1>
+      <h1 className='text-lg px-4'>Create your new Sneaker Plug account</h1>
       <Formik
         initialValues={initialValues}
         onSubmit={onSubmit}
         validationSchema={newAccountSchema}
       >
         {({ isSubmitting }) => (
-          <Form
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              padding: '2rem',
-              width: 'clamp(300px, 50vw, 550px)',
-              fontSize: '1.25rem',
-            }}
-          >
-            <label htmlFor='firstName'>First Name</label>
-            <Field style={FieldStyle} name='firstName' type='text' />
-            <ErrorMessage name='firstName' component='div' />
-            <label htmlFor='lastName'>Last Name</label>
-            <Field style={FieldStyle} name='lastName' type='text' />
-            <ErrorMessage name='lastName' component='div' />
-            <label htmlFor='email'>Email</label>
-            <Field style={FieldStyle} name='email' type='email' />
-            <ErrorMessage name='email' component='div' />
-            <label htmlFor='phone'>Phone</label>
-            <Field style={FieldStyle} name='phone' type='phone' />
-            <ErrorMessage name='phone' component='div' />
-            <label htmlFor='streetAddress'>Street Address</label>
-            <Field style={FieldStyle} name='streetAddress' type='text' />
-            <ErrorMessage name='streetAddress' component='div' />
-            <label htmlFor='streetAddress2'>Street Address 2</label>
-            <Field style={FieldStyle} name='streetAddress2' type='text' />
-            <ErrorMessage name='streetAddress2' component='div' />
-            <label htmlFor='city'>City</label>
-            <Field style={FieldStyle} name='city' type='text' />
-            <ErrorMessage name='city' component='div' />
-            <label htmlFor='state'>State</label>
-            <Field style={FieldStyle} name='state' type='text' />
-            <ErrorMessage name='state' component='div' />
-            <label htmlFor='zipCode'>Zip Code</label>
-            <Field style={FieldStyle} name='zipCode' type='number' />
-            <ErrorMessage name='zipCode' component='div' />
-            <button style={ButtonStyle} type='submit' disabled={isSubmitting}>
+          <Form className='flex flex-col gap-y-4 p-4 w-full text-xl'>
+            <div className='grid grid-cols-2 gap-x-4'>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='firstName'>First Name</label>
+                <Field
+                  className='border border-gray-500 px-4 rounded-xl mb-4 h-14'
+                  name='firstName'
+                  type='text'
+                />
+                <ErrorMessage name='firstName' component='div' />
+              </div>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='lastName'>Last Name</label>
+                <Field
+                  className='border border-gray-500 px-4 rounded-xl mb-4 h-14'
+                  name='lastName'
+                  type='text'
+                />
+                <ErrorMessage name='lastName' component='div' />
+              </div>
+            </div>
+            <div className='grid grid-cols-2 gap-4'>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='email'>Email</label>
+                <Field
+                  className='border border-gray-500 px-4 rounded-xl mb-4 h-14'
+                  name='email'
+                  type='email'
+                />
+                <ErrorMessage name='email' component='div' />
+              </div>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='phone'>Phone</label>
+                <Field
+                  className='border border-gray-500 px-4 rounded-xl mb-4 h-14'
+                  name='phone'
+                  type='phone'
+                />
+                <ErrorMessage name='phone' component='div' />
+              </div>
+            </div>
+            <div className='flex flex-col text-sm'>
+              <label htmlFor='streetAddress'>Street Address</label>
+              <Field
+                className='border border-gray-500 px-4 h-14 rounded-xl mb-4'
+                name='streetAddress'
+                type='text'
+              />
+              <ErrorMessage name='streetAddress' component='div' />
+            </div>
+            <div className='flex flex-col text-sm'>
+              <label htmlFor='streetAddress2'>Street Address 2</label>
+              <Field
+                className='border border-gray-500 px-4 h-14 rounded-xl mb-4'
+                name='streetAddress2'
+                type='text'
+              />
+              <ErrorMessage name='streetAddress2' component='div' />
+            </div>
+            <div className='grid grid-cols-3 gap-x-4'>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='city'>City</label>
+                <Field
+                  className='border border-gray-500 p-4 h-14 rounded-xl mb-4 text-xl'
+                  name='city'
+                  type='text'
+                />
+                <ErrorMessage name='city' component='div' />
+              </div>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='state'>State</label>
+                <Field
+                  className='border border-gray-500 p-4 h-14 rounded-xl mb-4 text-xl'
+                  name='state'
+                  type='text'
+                />
+                <ErrorMessage name='state' component='div' />
+              </div>
+              <div className='flex flex-col text-sm'>
+                <label htmlFor='zipCode'>Zip Code</label>
+                <Field
+                  className='border border-gray-500 p-4 h-14 rounded-xl mb-4 text-xl'
+                  name='zipCode'
+                  type='number'
+                />
+                <ErrorMessage name='zipCode' component='div' />
+              </div>
+            </div>
+            <button
+              className='bg-orange-400 rounded-lg text-white p-4 text-lg'
+              type='submit'
+              disabled={isSubmitting}
+            >
               Submit
             </button>
           </Form>
@@ -89,19 +150,3 @@ const CreateAccount = (props: Props) => {
 }
 
 export default CreateAccount
-
-const FieldStyle = {
-  padding: '1rem',
-  height: '2rem',
-  borderRadius: '8px',
-  margin: '0 0 1rem 0',
-  fontSize: '1.25rem',
-}
-
-const ButtonStyle = {
-  background: 'var(--primary-color)',
-  borderRadius: '8px',
-  color: 'white',
-  padding: '1rem',
-  fontSize: '1.25rem',
-}
