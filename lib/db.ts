df337da12974ff9e314fa592b34ab38084a4ad2a
Quincy Obeng <quincyobeng@gmail.com>
2023-02-10 09:33:06 -0600

    tiny: Minor DB handler update

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,24 @@
 import mysql from 'serverless-mysql'
 
 const db = mysql({
-    config: {
-        host: process.env.SNEAKERS_DB_HOST,
-        port: Number.parseInt(process.env.SNEAKERS_DB_PORT),
-        user: process.env.SNEAKERS_DB_USERNAME,
-        password: process.env.SNEAKERS_DB_PASSWORD,
-        database: process.env.SNEAKERS_DB_TABLE_NAME,
-    },
-    onConnectError: (error) => {
-        console.log({ DBError: error })
-    },
+  config: {
+    host: process.env.SNEAKERS_DB_HOST,
+    port: Number.parseInt(process.env.SNEAKERS_DB_PORT),
+    user: process.env.SNEAKERS_DB_USERNAME,
+    password: process.env.SNEAKERS_DB_PASSWORD,
+    database: process.env.SNEAKERS_DB_TABLE_NAME
+  },
+  onConnectError: (error) => {
+    console.log({ DBError: error })
+  }
 })
 
-export const queryDatabase = async (query: string, values?: string[]) => {
-    await db.connect()
-    const results = values
-        ? await db.query({ sql: query, values })
-        : await db.query(query)
-    await db.end()
-    return results
+export const queryDatabase = async (query: string, values?: any[]) => {
+  await db.connect()
+  const results = values
+    ? await db.query({ sql: query, values })
+    : await db.query(query)
+  await db.end()
+
+  return results
 }
