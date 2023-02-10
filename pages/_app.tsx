6863034cb96c0d655e24a4f3b4d74487c9dbd0e2
Quincy Obeng <quincyobeng@gmail.com>
2023-02-10 09:27:22 -0600

    feat: Update base app page to use admin layout for layout pages and update to The Snkr Plug with minor style change for home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
+import { useRouter } from 'next/router'
+import AdminLayout from '../modules/common/components/layouts/AdminLayout'
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const { pathname } = router
+
+  if (pathname.includes('/admin')) {
+    return (
+      <AdminLayout>
+        <Component {...pageProps} />
+      </AdminLayout>
+    )
+  }
+
   return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,37 +19,45 @@ const Home = ({ sneakers }: Props) => {
   const router = useRouter()
 
   return (
-    <Layout title='Home | Sneaker Plug'>
+    <Layout title='Home | The Snkr Plug'>
       <div>
-        <h1>Sneaker Plug</h1>
-        <ul>
-          {sneakers.map((sneaker) => (
-            <li
-              key={sneaker.id}
-              onClick={() => router.push(`/sneakers/view/${sneaker.id}`)}
-            >
-              <img
-                style={{
-                  maxWidth: '400px',
-                }}
-                src={`/api/sneakers/images/${sneaker.image}`}
-                alt={sneaker.name}
-              />
-              <h2>{sneaker.name}</h2>
-              <p>{sneaker.brand}</p>
-              <p>${sneaker.rate} per day</p>
-              <button onClick={() => addToCart(sneaker)}>Add to cart</button>
-            </li>
-          ))}
-        </ul>
-        <h2>Cart</h2>
-        <ul>
-          {cart.map((sneaker) => (
-            <li key={sneaker.id}>
-              <h3>{sneaker.name}</h3>
-            </li>
-          ))}
-        </ul>
+        <h1>The Snkr Plug</h1>
+        <div className='flex md:flex-col'>
+          <ul className='flex flex-wrap w-full lg:w-5/6 md:w-auto gap-8'>
+            {sneakers.map((sneaker) => (
+              <li
+                key={sneaker.id}
+                onClick={() => router.push(`/sneakers/view/${sneaker.id}`)}
+              >
+                <div className='shadow-lg rounded-lg flex flex-col gap-4 items-start border border-gray-700'>
+                  <img
+                    style={{
+                      maxWidth: '400px'
+                    }}
+                    src={`/api/sneakers/images/${sneaker.image}`}
+                    alt={sneaker.name}
+                  />
+                  <h2>{sneaker.name}</h2>
+                  <p>{sneaker.brand}</p>
+                  <p>${sneaker.rate} per day</p>
+                  <button onClick={() => addToCart(sneaker)}>
+                    Add to cart
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className='w-full lg:w-1/6'>
+            <h2>Cart</h2>
+            <ul>
+              {cart.map((sneaker) => (
+                <li key={sneaker.id}>
+                  <h3>{sneaker.name}</h3>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </Layout>
   )
@@ -64,14 +72,14 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
       brand: result.brand,
       rate: result.rate,
       date: result.date,
-      image: result.image,
+      image: result.image
     }
   })
 
   return {
     props: {
-      sneakers,
-    },
+      sneakers
+    }
   }
 }
 
