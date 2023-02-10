114865370050a09d05ac8dcacaa5ffa0d71b5b68
Quincy Obeng <quincyobeng@gmail.com>
2023-02-10 09:25:58 -0600

    chore: Update User object model

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -1,17 +1,19 @@
 type User = {
-    id: string
-    userName: string
-    password?: string
-    firstName: string
-    lastName: string
-    email: string
-    phone?: string
-    streetAddress?: string
-    streetAddress2?: string
-    city?: string
-    state?: string
-    zipCode?: string
-    isAdmin: boolean
+  id: string
+  username: string
+  password?: string
+  firstName: string
+  lastName: string
+  email: string
+  phone?: string
+  streetAddress?: string
+  streetAddress2?: string
+  city?: string
+  state?: string
+  zipCode?: string
+  emailVerified?: 0 | 1
+  isAdmin: 0 | 1
+  deletedAt?: string
 }
 
 export default User
